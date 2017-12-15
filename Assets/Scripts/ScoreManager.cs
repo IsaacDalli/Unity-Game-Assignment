d6c765c1f63a457aef7a4059ce4451331326c4d0
IsaacDalli <IsaacDalli@users.noreply.github.com>
2017-12-15 22:29:36 +0200

    Game Manager
    -Added a Game manager
    -Added script to the game Manager to control scores
    -Scores increase when a player scores

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+	private int P1 = 0;
+	private int P2 = 0;
+	public Text Ply1Score;
+	public Text Ply2Score;
+	public GameObject ball;
+	void Start () {
+			
+	}
+
+	void Update () {
+		if (ball.transform.position.x >= 22){
+			P1++;
+			Ply1Score.text = P1.ToString();
+		}else if (ball.transform.position.x <= -22){
+			P2++;
+			Ply2Score.text = P2.ToString();
+		}
+	}
+}
