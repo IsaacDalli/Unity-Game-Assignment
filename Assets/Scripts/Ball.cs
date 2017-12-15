9aa7c3b470467f6fbeb8bbef97b53550c7fd6754
IsaacDalli <IsaacDalli@users.noreply.github.com>
2017-12-15 20:26:48 +0200

    Paddles and Ball
    -Added Ball Movement
    -Added Paddles Movement (Keyboard and Mouse)
    -Added Borders
    -Added Reset ball function to go back in the middle
    -Added Bounciness to ball
    -Added Colliders to Borders

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+		rb.velocity = new Vector2 (7f, 7f);
+	}
+
+	void Update () {
+		if (this.transform.position.x >= 22) {
+			this.transform.position = new Vector3 (0f,0f,0f);
+		} else if (this.transform.position.x <= 22) {
+			this.transform.position = new Vector3 (0f,0f,0f);
+		}
+
+	}
+}
---
+++ Assets/Scripts/Paddle1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle1 : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			rb.velocity = new Vector2 (0f, 6f);
+		} else if (Input.GetKey (KeyCode.S)) {
+			rb.velocity = new Vector2 (0f, -6f);
+		} else {
+			rb.velocity = new Vector2 (0f, 0f);
+		}
+	}
+}
